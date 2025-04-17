4a93e7f6b494692b5afbc5d2cf51487bc64d7bda
rilso <carvalho.railson262@gmail.com>
2025-04-17 00:38:33 -0300

    Subindo projeto completo

--- src/test/java/com/desafio/pacto/recrutamento/DesafioPactoApplicationTests.java
+++ src/test/java/com/desafio/pacto/recrutamento/DesafioPactoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DesafioPactoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
